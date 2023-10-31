cc948d34424d6a98e1206f9d8798f5099a317ba6
rumi0221 <148302664+rumi0221@users.noreply.github.com>
2023-10-31 14:57:29 +0900

    登録確認画面PHP　途中まで

---
+++ USER/singnup-check.php
@@ -0,0 +1,10 @@
+<?php
+echo '名前', '<br>';
+echo $_POST['name'], '<br>';
+echo '名前(フリガナ)', '<br>';
+echo $_POST['name1'], '<br>';
+echo 'メールアドレス', '<br>';
+echo $_POST['email'], '<br>';
+echo 'パスワード', '<br>';
+echo $_POST['password'], '<br>';
+?>
