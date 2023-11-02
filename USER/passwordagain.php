a6aa091513909d3010ea8dcc27d1ac8e13b5ea0e
fireohiro <148302631+fireohiro@users.noreply.github.com>
2023-11-02 12:47:10 +0900

    a

--- USER/passwordagain.php
+++ USER/passwordagain.php
@@ -1,7 +1,12 @@
-<?php require 'db-connect.php'; ?>
 <?php
 echo '<h4>パスワード再設定</h4><br>';
 echo '<form action="passwordagain-ok.php" method="post">'
-echo 'メールアドレス<br>';
+echo '<p>メールアドレス</p>';
 echo '<input type="text" name="mail"><br><br>';
-echo '新しいパスワード<br>';
+echo '<p>新しいパスワード</p>';
+echo '<input type="pass" name="newpass"><br><br>';
+echo '<p>新しいパスワード（確認）</p>';
+echo '<input type="pass" name="newpassok">';
+echo '<button type="submit">登録</button>';
+echo '</form>';
+echo ''
