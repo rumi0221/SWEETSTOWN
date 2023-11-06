67055108b39eb8fe59923aea11fea251f150df6b
Reiji717 <2201192@s.asojuku.ac.jp>
2023-11-06 12:31:02 +0900

    a

--- USER/signup-ok.php
+++ USER/signup-ok.php
@@ -9,8 +9,8 @@
 <body>
   <form action="login.php" method="post">
     <h1>登録が完了しました</1><br>
-    <p><img= src="img./check.png" alt="check"></p>
-    <p><img alt="image" src="image/'check.jpg"></p>
+
+    <p><img alt="image" src="image/ check.png"></p>
     <button type="submit">ログイン画面へ</button>
   </form>
 </body>
