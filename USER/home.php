72b97a62fd906885c49a53f1ad67ebc39fb8c600
fireohiro <148302631+fireohiro@users.noreply.github.com>
2023-11-07 14:58:07 +0900

    Update home.php

--- USER/home.php
+++ USER/home.php
@@ -1,30 +1,27 @@
 <?php session_start();?>
 <?php require 'db-connect.php'; ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/header.css">
-
-    <title>ホーム画面</title>
-</head>
-<body>
-    <?php
+<?php
     unset($_SESSION['member']);
     $pdo=new PDO($connect,USER,PASS);
     $sql=$pdo->prepare('select * from member where mail=? and pass=?');
     $sql->execute([$_POST['mail'],$_POST['pass']]);
     $count = $sql -> rowCount();
-    if($count != 0){
+    if($count == 0){
+        header('http://aso2301331.noor.jp/Githab/ejima/SWEETSTOWN/USER/login-input.php');
+        exit;
+    }else{
+        echo '<!DOCTYPE html>';
+        echo '<html lang="ja">';
+        echo '<head>';
+        echo '<link rel="stylesheet" href="css/header.css">';
+
+        echo '<title>ホーム画面</title>';
+        echo '</head>';
+        echo '<body>';
+
+        echo '</body>';
+        echo '</html>';
         echo '<div class="Header">SWEETSTOWN</div><br>';
         echo '<div><br><a href="seasonlist.php"><img src="img/season.jpg"></a><br></div><br>';
-    }else{
-        header('https://aso2301331.noor.jp/Githab/ejima/SWEETSTOWN/USER/login-input.php');
-        exit;
     }
-    ?>
-    
-
-</body>
-</html>
+?>
