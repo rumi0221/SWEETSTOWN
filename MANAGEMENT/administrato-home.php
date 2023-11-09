24a19a22184497882b89ee01f35b80a302fe19cc
rumi0221 <148302664+rumi0221@users.noreply.github.com>
2023-11-09 12:54:17 +0900

    管理者側の画面作成中

--- MANAGEMENT/administrato-home.php
+++ MANAGEMENT/administrato-home.php
@@ -7,7 +7,7 @@
     <title>管理者ホーム画面</title>
 </head>
 <body>
-    <p><a href="customerlist.html">顧客管理</a></p>
+    <p><a href="customerlist.php">顧客管理</a></p>
     <p><a href="productlist.html">商品登録</a></p>
     <p><a href="overall-sales.html">売上管理</a></p>
     <p><a href="order-management.html">受注管理</a></p>
