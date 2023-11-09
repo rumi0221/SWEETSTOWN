02d6d25ce3c64017b7c44b9218ea237ee4c96376
fireohiro <148302631+fireohiro@users.noreply.github.com>
2023-11-09 12:47:54 +0900

    no

--- USER/home.php
+++ USER/home.php
@@ -21,8 +21,22 @@
         echo '<div><br><a href="seasonlist.php"><img src="img/season.jpg"></a><br></div><br>';
         echo '<br>';
         echo '<form action="cart.php" method="post">';
-        echo '<button type="submit">カートない商品一覧</button>';
-        echo '</form>';
+        echo '<button type="submit">カート内商品一覧</button>';
+        echo '</form><br>';
+        $sql=$pdo->query('select * from product');
+        $count=0;
+        foreach($sql as $row){
+            $count++;
+            echo '<div class = "product_item">';
+            echo '<a href="detail.php"><img src="img/',$row['gazou'],'"></a>';
+            echo '<br>';
+            cho "<span>",$row['product_mei'],$row['tanka'],"</span>";
+            if($count==3){
+                echo '<br>';
+                $count=0;
+            }
+            echo '</div>';
+        }
         echo '</body>';
         echo '</html>';
     }
