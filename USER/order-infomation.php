3317b19f502b3f9efacb911f4d46a2abf757dae2
fireohiro <148302631+fireohiro@users.noreply.github.com>
2023-11-13 12:20:36 +0900

    Update order-infomation.php

--- USER/order-infomation.php
+++ USER/order-infomation.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/header.css">
     <title>注文情報入力画面</title>
 </head>
 <body>
-    <div class="Header">
-        <link rel="stylesheet" href="css/header.css">
-        SWEETSTOWN
-    </div>
-
+    <div class="Header">SWEETSTOWN</div>
     <link rel="stylesheet" href="css/cart.css">
     <br><br>
 
@@ -48,13 +46,12 @@
     </div>
     <br>
     <u><h3>お届け場所</h3></u>
-    <div class="radio">
-        <input type="radio" id="指定なし" name="delivery" value="指定なし" checked />
-        <label for="指定なし">指定なし　　　？月？日～？月？日発送予定</label><hr>
-    </div>
-    <div class="radio">
-        <input type="radio" id="日時指定" name="delivery" value="日時指定"/>
-        <label for="日時指定">日時指定</label><hr>
-    </div>
+    <div class="place">
+        <dt>郵便番号(ハイフンなし)</dt>
+            <dd>〒<input type="number" name = "place" size="10" maxlength="7" required="required"></dd>
+        <dt>住所<dt>
+            <dd>　<textarea rows="2" width="100" name = "place" size="30" required="required"></textarea></dd>
+    </div><br>
+    <button class="button2" onclick="location.href='order-check.html'">注文確認</button>
 </body>
 </html>
