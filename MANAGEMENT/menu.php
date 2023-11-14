c6b562c14054ff834c92c828d445f5a76de6dcce
rumi0221 <148302664+rumi0221@users.noreply.github.com>
2023-11-14 12:46:43 +0900

    サイドメニューバーのcss完成

--- MANAGEMENT/menu.php
+++ MANAGEMENT/menu.php
@@ -1,5 +1,8 @@
-<a href="customerlist.php">顧客管理</a>
-<a href="productlist.php">商品登録</a>
-<a href="overall-sales.php">売上管理</a>
-<a href="order-management.php">受注管理</a>
-<a href="publish.php">掲載</a>
+
+<div class="side">
+    <a href="customerlist.php" class="menu-btn">顧客管理</a>
+    <a href="productlist.php" class="menu-btn">商品登録</a>
+    <a href="overall-sales.php" class="menu-btn">売上管理</a>
+    <a href="order-management.php" class="menu-btn">受注管理</a>
+    <a href="publish.php" class="menu-btn">　掲載　</a>
+</div>
