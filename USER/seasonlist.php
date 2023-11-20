5131e2f56c6dd2c142beb11db5f0deb9537aee9f
Nrharuka <2201169@s.asojuku.ac.jp>
2023-11-20 12:47:08 +0900

    季節のスイーツ一覧画面PHP作成中

---
+++ USER/seasonlist.php
@@ -0,0 +1,35 @@
+<?php require 'db-connect.php'; ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>季節のスイーツ一覧画面</title>
+</head>
+<body>
+    <div class="Header">
+        <link rel="stylesheet" href="header.css">
+        SWEETSTOWN
+    </div>
+
+    <link rel="stylesheet" href="seasonlist.css">
+    <br><br>
+
+    <h1>季節のスイーツ</h1>
+    <hr width="90%" noshade><br>
+
+    <div class="seasonlist">
+    <a class="image"><image src="img/sweet.png"></href></a>
+        <section>
+                　商品名<br>
+                　ショップ名<br>
+                　￥
+            <br><br>
+        </section></div>
+
+    <br><br><br><br><br>
+
+    <footer><?php require 'menu.php';?></footer>
+</body>
+</html>
