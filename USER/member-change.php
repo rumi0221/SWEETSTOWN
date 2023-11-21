f36c6835cd5f1d60551dece5d99b15b5657b65cb
fireohiro <148302631+fireohiro@users.noreply.github.com>
2023-11-21 14:26:31 +0900

    a

--- USER/member-change.php
+++ USER/member-change.php
@@ -59,13 +59,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </form>
 
     <br><br><br><br><br>
-    <div class="menu">
-        <hr>
-        <a href="home.php"><img src="img/home.png"></a>
-        <a href="favorite-sweet.php"><img src="img/favorite.png"></a>
-        <a href="search.php"><img src="img/search.png"></a>
-        <a href="ranking.php"><img src="img/rank.png"></a>
-        <a href="others.php"><img src="img/else.png"></a>
-    </div>
+    <footer><?php require 'menu.php';?></footer>
 </body>
 </html>
---
+++ USER/purchase-history.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>購入履歴画面</title>
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/purchase-history.css">
+</head>
+<body>
+    <div class="Header">
+        SWEETSTOWN
+      </div>
+     <div class="hed">
+        <h1>購入履歴</h1>
+    <hr size="1">
+</div>
+<div class="shohin">
+    <img src="" alt="商品画像">
+    <div class="shohin-rireki">
+        <p>商品名</p>
+        <p>購入日時:YYYY/MM/DD</p>
+        <p>発送準備中・発送済み</p>
+    </div>
+    <div class="button">
+        <button class="button" onclick="location.href='review.html'">レビューを書く</button>
+    </div>
+</div>
+</body>
+</html>
