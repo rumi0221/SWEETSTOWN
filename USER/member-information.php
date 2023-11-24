2d6583cf6af01ec872b252cfb9cba85c285af41e
Reiji717 <2201192@s.asojuku.ac.jp>
2023-11-24 12:51:27 +0900

    update

--- USER/member-information.php
+++ USER/member-information.php
@@ -9,8 +9,8 @@ require 'db-connect.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>会員情報画面</title>
-    <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/member.css">
+    <link rel="stylesheet" href="CSS/header.css">
+    <link rel="stylesheet" href="CSS/member.css">
 </head>
 <body>
     <div class="Header">
