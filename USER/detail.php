d590994ed01fa515f2462f3ea2ab5087ebddf05b
fireohiro <148302631+fireohiro@users.noreply.github.com>
2023-11-24 15:00:54 +0900

    Update detail.php

--- USER/detail.php
+++ USER/detail.php
@@ -17,39 +17,36 @@
         $pdo=new PDO($connect,USER,PASS);
         $sql=$pdo->prepare('select * from product where product_id=?');
         $sql->execute([$_GET['product_id']]);
-        $i = '♡';
         foreach($sql as $row){
-            echo '<div class="gazou">';
-            echo '<img src="img/',$row['gazou'],'" alt="商品画像">';
-            echo '</div>';
-            echo '<div class="shohin1">';
-            echo '<h3>',$row['product_mei'],'</h3>';
-            echo '<p>',$row['tanka'],'</p>';
-            $ssl=$pdo->prepare('select * from shop where shop_code=?');
-            $ssl->execute([$row['shop_code']]);
-            foreach($ssl as $eow){
-                echo '<p>',$eow['shop_mei'],'</p>';
-            }
-            echo '</div>';
-            echo '<form method="post">';
-            echo '<button type="submit" name="favorite">',$i,'</button>';
-            echo '</form>';
-            echo '<div class="shohin2">';
-            echo '<button type="input-cart" onclick="location.href=cart.php">🛒 カートに入れる</button>';
-            echo '<p>',$row['setumei'],'</p>';
-            echo '<button type="review"onclick="location.href="review.html"">レビュー</button>';
-            $spl=$pdo->prepare('select * from product where product_id <> ? and product_type = ?');
-            $spl->execute([$row['product_id'],$row['product_type']]);
-            foreach($spl as $mow){
-              echo '<a href="detail.php?product_id=<img src="img/',$mow['gazou'],'"'
-            }
+          $fav=$pdo->prepare('select * from favorite where member_id = ? and product_id = ?');
+          $fav->execute([$_SESSION['member']['member_id'],$row['product_id']]);
+          echo '<div class="gazou">';
+          echo '<img src="img/',$row['gazou'],'" alt="商品画像">';
+          echo '</div>';
+          echo '<div class="shohin1">';
+          echo '<h3>',$row['product_mei'],'</h3>';
+          echo '<p>',$row['tanka'],'</p>';
+          $ssl=$pdo->prepare('select * from shop where shop_code=?');
+          $ssl->execute([$row['shop_code']]);
+          foreach($ssl as $eow){
+              echo '<p>',$eow['shop_mei'],'</p>';
+          }
+          echo '</div>';
+          echo '<form method="post">';
+          echo '<button type="submit" name="favorite">',$i,'</button>';
+          echo '</form>';
+          echo '<div class="shohin2">';
+          echo '<button type="input-cart" onclick="location.href=cart.php">🛒 カートに入れる</button>';
+          echo '<p>',$row['setumei'],'</p>';
+          echo '<button type="review"onclick="location.href="review.html"">レビュー</button>';
+          $spl=$pdo->prepare('select * from product where product_id <> ? and product_type = ?');
+          $spl->execute([$row['product_id'],$row['product_type']]);
+          foreach($spl as $mow){
+            echo '<a href="detail.php?product_id=',$mow['product_id'],'><img src="img/',$mow['gazou'],'"></a>';
+          }
+          echo '</div>';
         }
       ?>
-        <p>商品説明</p>
-        <button type="review"onclick="location.href='review.html'">レビュー</button>
-        <p>関連商品</p>
-      </div>
-
-
+      <footer><?php require 'menu.php';?></footer>
 </body>
 </html>
