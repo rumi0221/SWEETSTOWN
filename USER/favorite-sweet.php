7457c3733bdd569b0070dc576782da8944ffe0d2
hikaru0214 <hikaru.yoshida.0214@gmail.com>
2023-11-27 12:45:19 +0900

    作成

---
+++ USER/favorite-sweet.php
@@ -0,0 +1,16 @@
+<?php session_start();?>
+<?php require 'db-connect.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/favorite-sweet.css">
+    <title>お気に入り画面(スイーツ)</title>
+</head>
+<body>
+    <div class="Header">
+        SWEETSTOWN
+    </div>
