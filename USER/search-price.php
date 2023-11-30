905dd766bb57ff1665ecffb532f185f34c67887a
Reiji717 <2201192@s.asojuku.ac.jp>
2023-11-30 11:28:34 +0900

    search

---
+++ USER/search-price.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+require 'db-connect.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['price_range'])) {
+    $min_price = 0;
+    $max_price = 0;
+
+    
+        switch ($_POST['price_range']) {
+            case '100-500':
+                $min_price = 100;
+                $max_price = 500;
+                break;
+            case '500-1000':
+                $min_price = 500;
+                $max_price = 1000;
+                break;
+            case '1000-1500':
+                $min_price = 1000;
+                $max_price = 1500;
+                break;
+            case '1500-2000':
+                $min_price = 1500;
+                $max_price = 2000;
+                break;
+            case '2000-2500':
+                $min_price = 2000;
+                $max_price = 2500;
+                break;
+            case '2500-3000':
+                $min_price = 2500;
+                $max_price = 3000;
+                break;
+            default:
+               
+                break;
+        }
+
+        try {
+            $stmt = $pdo->prepare('SELECT product_id, product_mei, shop_code, tanka, gazpu 
+                                   FROM product
+                                   WHERE tanka BETWEEN :min_price AND :max_price');
+            $stmt->execute(['min_price' => $min_price, 'max_price' => $max_price]);
+            $search_result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            
+            $_SESSION['min_price'] = $min_price;
+            $_SESSION['max_price'] = $max_price;
+            $_SESSION['search_result'] = $search_result;
+            header("Location: searchlist.php");
+            exit();
+        } catch (PDOException $e) {
+            echo "エラー：" . $e->getMessage();
+            exit();
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/header.css">
+    <link rel="stylesheet" href="CSS/search.css">
+    <title>検索画面</title>
+</head>
+<body>
+    <div class="Header">
+        SWEETSTOWN
+    </div>
+    <div class="search">
+        <form action="searchlist.php" method="post">
+            <button class="searchbutton" name="price_range" value="100-500">￥100 ～ 500</button>
+            <button class="searchbutton" name="price_range" value="500-1000">￥500 ～ 1000</button>
+            <button class="searchbutton" name="price_range" value="1000-1500">￥1000 ～ 1500</button>
+            <button class="searchbutton" name="price_range" value="1500-2000">￥1500 ～ 2000</button>
+            <button class="searchbutton" name="price_range" value="2000-2500">￥2000 ～ 2500</button>
+            <button class="searchbutton" name="price_range" value="2500-3000">￥2500 ～ 3000</button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ USER/searchlist.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+require 'db-connect.php'; 
+
+$min_price = isset($_SESSION['min_price']) ? $_SESSION['min_price'] : 0;
+$max_price = isset($_SESSION['max_price']) ? $_SESSION['max_price'] : PHP_INT_MAX;
+$search_result = isset($_SESSION['search_result']) ? $_SESSION['search_result'] : [];
+
+
+$stmt = $pdo->prepare('SELECT product_id, product_mei, shop_code, tanka, gazou 
+                       FROM product 
+                       WHERE tanka BETWEEN :min_price AND :max_price');
+$stmt->execute(['min_price' => $min_price, 'max_price' => $max_price]);
+$search_result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/search.css">
+    <link rel="stylesheet" href="CSS/header.css">
+    <title>検索結果一覧画面</title>
+</head>
+
+<body>
+    <div class="Header">
+      SWEETSTOWN
+    </div>
+    <div class="search">
+        <form action="searchlist.php" method="post">
+            <input type="text" name="search" placeholder="🔍 キーワード検索" />
+            <p><button type="submit">検索</button></p>
+        </form>
+    </div>
+
+    <?php foreach ($search_result as $product) : ?>
+        <div class="Shohin">
+            <img src="<?php echo $product['gazou']; ?>" >
+            <div class="shohin-setumei">
+                <p><?php echo $product['product_mei']; ?></p>
+                <p><?php echo $product['shop_code']; ?></p>
+                ￥<?php echo $product['tanka']; ?>
+                <br>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</body>
+
+</html>
