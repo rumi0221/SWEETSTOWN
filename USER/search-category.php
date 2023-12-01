1d83601b034d49f11465da0deb2d1ba793701532
fireohiro <148302631+fireohiro@users.noreply.github.com>
2023-12-01 10:28:45 +0900

    カテゴリサーチ

--- USER/search-category.php
+++ USER/search-category.php
@@ -7,22 +7,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/header.css">
     <link rel="stylesheet" href="CSS/search.css">
-
+    <link rel="stylesheet" href="CSS/menu.css">
     <title>検索画面</title>
 </head>
 <body>
     <div class="Header">
         SWEETSTOWN
     </div>
+    <div class="content">
         <?php
             $pdo=new PDO($connect,USER,PASS);
             $sql=$pdo->query('select distinct product_type FROM product');
             foreach($sql as $row){
                 echo '<div class="search">';
-                echo '<button onclick="location.href=\'searchlist.php?id=' . $row['product_type'] . '\'">'.$row['product_type'].'</button><br>';
+                echo '<button onclick="location.href=\'searchlist.php?id=' . $row['product_type'] . '\'" style="margin-top:50px;">'.$row['product_type'].'</button><br>';
                 echo '</div>';
             }
         ?>
+    </div>
     <footer><?php require 'menu.php';?></footer>
 </body>
 </html>
