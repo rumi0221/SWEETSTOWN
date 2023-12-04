c5f152ee8cbe50e1ec1ba560b24e66520ed8c346
hk0308 <148302687+hk0308@users.noreply.github.com>
2023-12-04 11:39:48 +0900

    レビュー一覧画面作成中

--- USER/reviewlist.php
+++ USER/reviewlist.php
@@ -22,16 +22,15 @@
         echo '</p>';
 
     }
-?>」
-\
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>レビュー一覧画面</title>
-    <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/review.css">
+    <link rel="stylesheet" href="CSS/header.css">
+    <link rel="stylesheet" href="CSS/review.css">
 </head>
 <body>
      <div class="Header">
