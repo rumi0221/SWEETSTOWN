fb56d55db5171dd310a2ff1e1b704016d87eb5ae
rumi0221 <148302664+rumi0221@users.noreply.github.com>
2023-12-06 11:48:16 +0900

    受注管理画面途中

--- MANAGEMENT/customer-infomation.php
+++ MANAGEMENT/customer-infomation.php
@@ -28,11 +28,14 @@
                     foreach($sql as $row){
                         $id = $row['product_id'];
                         $sql2 = $pdo->query('select * from product where product_id = '. $_POST['id']);
+                        $row2 = $sql2->fetch(PDO::FETCH_BOTH, PDO::FETCH_ORI_LAST);
                         echo '<tr>';
-                        echo '<td>', $sql2['product_id'], '</td>';
-                        echo '<td>', $sql2['product_mei'], '</td>';
-                        echo '<td>', $pdo->query('select * from shop where shop_code = '. $sql2['shop_code']), '</td>';
-                        echo '<td>', $sql2['tanka'], '</td>';
+                        echo '<td>', $row2['product_id'], '</td>';
+                        echo '<td>', $row2['product_mei'], '</td>';
+                        $sql3 = $pdo->query('select * from shop where shop_code = '. $sql2['shop_code']);
+                        $row3 = $sql3->fetch(PDO::FETCH_BOTH, PDO::FETCH_ORI_LAST);
+                        echo '<td>', $row3['shop_mei'], '</td>';
+                        echo '<td>', $row2['tanka'], '</td>';
                         echo '<td>', $row['su'], '</td>';
                         echo '</tr>';
                     }
--- MANAGEMENT/shipping-check.php
+++ MANAGEMENT/shipping-check.php
@@ -4,12 +4,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/main.css">
+    <link rel="stylesheet" href="CSS/order.css">
     <title>発送確認画面</title>
 </head>
 <body>
     <div class="main">
-     
+        <h1>発送確認</h1>
+        <table>
+            <tr>
+                <th>受注日</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>注文番号</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>顧客番号</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>お支払い方法</th>
+                <td></td>
+            </tr>
+                <th>お届け場所</th>
+                <td></td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <th>商品ID</th><th>単価</th><th>購入数</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+        
+        <p>商品合計　￥</p>
+
+        <button>発送</button>
+
+        <a href="productlist.php">戻る</a>
+
     </div>
 </body>
 </html>
