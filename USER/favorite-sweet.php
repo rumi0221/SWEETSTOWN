457308f95940181b92732db78338c8169062c408
Reiji717 <2201192@s.asojuku.ac.jp>
2023-12-07 12:18:50 +0900

    menuの統一

--- USER/favorite-sweet.php
+++ USER/favorite-sweet.php
@@ -50,14 +50,7 @@ require 'db-connect.php';
         ?> 
         </div>
         </div>
-    <div class="menu">
-        <hr>
-            <a href="home.php"><img src="img/home.png"></a>
-            <a href="favorite-sweet.php"><img src="img/favorite.png"></a>
-            <a href="search.php"><img src="img/search.png"></a>
-            <a href="ranking.php"><img src="img/rank.png"></a>
-            <a href="others.php"><img src="img/else.png"></a>
-    </div> 
+        <hr> 
+    <footer><?php require 'menu.php';?></footer>
 </body>
-
 </html>
