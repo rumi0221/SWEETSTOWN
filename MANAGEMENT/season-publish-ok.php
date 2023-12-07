458b953fb3364d34c19abcd275e0ec965da1bb94
rumi0221 <148302664+rumi0221@users.noreply.github.com>
2023-12-07 12:41:05 +0900

    ランキング画面100%

--- MANAGEMENT/season-publish-ok.php
+++ MANAGEMENT/season-publish-ok.php
@@ -1,3 +1,4 @@
+<?php require 'db-connect.php'; ?>
 <?php require 'menu.php'; ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -10,6 +11,11 @@
 <body>
     <div class="main">
         <div class="link">
+            <?php
+                $pdo=new PDO($connect,USER,PASS);
+                $sql=$pdo->query('update product set season_flg = 0');
+                $sql2=$pdo->query('update product set season_flg = 1 where season = "'.$_POST['sflg'].'"');
+            ?>
                 <p>季節商品の掲載が完了しました</p>
                 <br>
                 <a href="administrato-home.php">ホームへ</a>
