b9bb42c326482854bf769ae6e788d5ab86b0bc68
hk0308 <148302687+hk0308@users.noreply.github.com>
2023-12-11 12:27:31 +0900

    レビュー

--- USER/review-ok.php
+++ USER/review-ok.php
@@ -16,13 +16,16 @@
     $sql2=$pdo->prepare('select count(*) from review where member_id = ?');
     $sql2->execute([$_SESSION['member']['member_id']]);
     $count = $sql2->fetchColumn();
-    var_dump($count);
-    echo $count;
-    exit;
+
+    // データベースにレビューがない場合：登録処理
+    if($count == 0){
+        
+        $sql=$pdo->prepare('INSERT INTO review values(?,?,?,?,?)');
+        $sql->execute([$_SESSION['member']['member_id'],$_POST['rate'],$_POST['product_id'],$_POST['title'],$_POST['review']]);
+
+    }
 
 
-    $sql=$pdo->prepare('INSERT INTO review values(?,?,?,?,?)');
-    $sql->execute([$_SESSION['member']['member_id'],$_POST['rate'],$_POST['product_id'],$_POST['title'],$_POST['review']]);
 ?>
 <body>
 
@@ -31,8 +34,12 @@
     </div>
 
     <div class="review-ok">
-        <p>レビューが投稿されました</p>
-
+        <?php if ($count == 0){
+            echo '<p>レビューが投稿されました</p>';
+        } else {
+            echo '<p>この商品に対するあなたのレビューはすでにされています。</p>';
+        }
+        ?>
         <a href="purchase-history.php">購入履歴に戻る</a>
     </div>
 
