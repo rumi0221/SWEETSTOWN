07470a21a2b16f49d3258290295f69f79516cacf
hk0308 <148302687+hk0308@users.noreply.github.com>
2023-12-11 12:17:39 +0900

    レビュー

--- USER/review-ok.php
+++ USER/review-ok.php
@@ -15,7 +15,7 @@
 
     $sql2=$pdo->prepare('select count(*) from review where member_id = ?');
     $sql2->execute([$_SESSION['member']['member_id']]);
-    var_dump($sql2->fetch());
+    $count = $sql2->fetchClolumn()
     exit;
 
 
