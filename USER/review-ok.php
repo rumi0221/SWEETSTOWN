e78f2397b787954fd242cf20f3e50315daa2f635
hk0308 <148302687+hk0308@users.noreply.github.com>
2023-12-11 12:44:20 +0900

    re

--- USER/review-ok.php
+++ USER/review-ok.php
@@ -20,14 +20,15 @@
     // データベースにレビューがない場合：登録処理
     if($count == 0){
         
-        $sql=$pdo->prepare('INSERT INTO review values(?,?,?,?,?)');
-        $sql->execute([$_SESSION['member']['member_id'],$_POST['rate'],$_POST['product_id'],$_POST['title'],$_POST['review']]);
         var_dump($_SESSION['member']['member_id']);
         var_dump($_POST['rate']);
         var_dump($_POST['product_id']);
         var_dump($_POST['title']);
         var_dump($_POST['review']);
         exit;
+        $sql=$pdo->prepare('INSERT INTO review values(?,?,?,?,?)');
+        $sql->execute([$_SESSION['member']['member_id'],$_POST['rate'],$_POST['product_id'],$_POST['title'],$_POST['review']]);
+
 
 
     }
